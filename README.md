1c0f67bef93bf69bd20edae3263676ffcc34082f
Camilo Santos <camilossantos.2809@gmail.com>
2017-05-08 10:20:45 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rpoperacoes
+
+Comandos SQL utilizados para auxiliar a implantação do sistema ERP ou correção de problemas comuns no suporte ao cliente.
---
+++ insert_tabemb.sql
@@ -0,0 +1,18 @@
+﻿/*
+  Insere os tipos de embalagens dos produtos
+  que não existem na tabela tabemb.
+*/
+insert into tabemb (
+  temb_codigo
+)
+select 
+  prod_emb
+from 
+  produtos
+group by 
+  prod_emb
+having 
+  prod_emb not in (
+    select temb_codigo
+    from tabemb
+  );
