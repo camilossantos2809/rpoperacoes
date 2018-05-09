5fc49b2d408f4c9a5b43302899c50d4cfe2e7869
Camilo Santos <camilossantos.2809@gmail.com>
2018-05-09 16:06:43 -0300

    Correções e melhorias na documentação.

--- view_delete_backup.sql
+++ view_delete_backup.sql
@@ -1,3 +1,16 @@
+/**
+    Remove todas as views do schema public criando um backup/histórico dos comandos SQL
+    de cada view na tabela backup.view.
+
+    A partir da versão 3.20 do FlexDB será necessária a exclusão das views antes da
+    atualização da estrutura. De qualquer maneira, o comando pode ser utilizado em
+    situações onde seja necessário eliminar de forma automática as views do schema public.
+
+    Para restaurar as views com base no backup gerado pelo código abaixo deverá 
+    ser utilizado o código do arquivo view_restore.sql, deste repositório.
+    https://github.com/camilossantos2809/rpoperacoes/blob/master/view_restore.sql
+**/
+
 do $$
     declare
         v_nome_view text;
@@ -14,12 +27,13 @@ do $$
             query text
         );
 
+        raise notice 'Inserindo dados das views na tabela backup.view';
         with views as (
-            insert into backup.view (oid, data_execucao, nome, query)
+            insert into backup.view (view_oid, data_execucao, nome, query)
             select oid, current_timestamp, relname, pg_get_viewdef(oid)
             from pg_class 
             where
-                relnamespace='public'::regnamespace
+                relnamespace='public'::regnamespace 
                 and relkind='v'
             returning nome
         )
@@ -27,9 +41,11 @@ do $$
         into v_nome_views
         from views;
 
+        raise notice 'Iniciando comandos para remoção de views do schema public';
         foreach v_nome_view in array v_nome_views loop
-            raise notice 'drop views if exists % cascade;', v_nome_view;
+            raise notice 'Removendo view % ...', v_nome_view;
             execute format('drop view if exists %I cascade;', v_nome_view);
+            raise notice 'Comando de exclusão da view % executado', v_nome_view;
         end loop;
     end
 $$ language plpgsql;
--- view_restore.sql
+++ view_restore.sql
@@ -1,3 +1,16 @@
+/**
+    Cria views com base em comandos SQL gravados na tabela backup.view.
+
+    Para utilização desse código, presume-se que o arquivo view_delete_backup.sql,
+    deste repositório, tenha sido executado anteriormente, gerando os dados necessários
+    na tabela backup.view.
+    https://github.com/camilossantos2809/rpoperacoes/blob/master/view_delete_backup.sql
+
+    Na tabela backup.view podem existir mais de um registro com o mesmo nome, a 
+    restauração será gerada considerando o registro que tenha o maior valor 
+    no campo data_execucao.
+**/
+
 do $$
     declare
         v_registros record;
@@ -16,7 +29,7 @@ do $$
                     ult_nome=nome
                     and ult_data=data_execucao
                 )
-            order by oid
+            order by view_oid
         loop
             raise notice 'Criando view % ...', v_registros.ult_nome;
             execute format('create or replace view %s as %s;', v_registros.ult_nome, v_registros.query);
