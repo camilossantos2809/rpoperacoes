7d394f04f499163b7f2c4b9d77f70e5b8aba69c8
Camilo Santos <camilossantos.2809@gmail.com>
2018-07-13 19:12:22 -0300

    Correção de "alias" das tabelas e melhoria na documentação

--- copia_info_produn.sql
+++ copia_info_produn.sql
@@ -1,15 +1,19 @@
 /*
-    Copia informações de produtos de uma unidade para a outra
+    Atualiza informações na tabela produn de uma unidade com base em uma outra.
+    
+    Pode ser útil em casos onde na implantação o cliente decide que informações de status
+    do produto, ou custos e preços, da matriz sejam atribuídos para unidades filiais.
+    Também pode ser útil quando houver a criação de mais unidades no sistema.
 
-    alt = Registros da produn que serão modificados
-    cop = Registros da produn que serão copiados/consultados
+    alt = Registros da produn que serão modificados (destino)
+    cop = Registros da produn que serão copiados/consultados (origem)
 */
 update produn as alt
 set 
     prun_ativo = cop.prun_ativo,
     prun_bloqueado = cop.prun_bloqueado
-from  produn as cop
+from produn as cop
 where 
-    prun1.prun_prod_codigo = prun2.prun_prod_codigo
-    and alt.prun_unid_codigo = '003' -- Informar o código da unidade que será alterada
+    alt.prun_prod_codigo = cop.prun_prod_codigo
+    and alt.prun_unid_codigo = '003' -- Informar o código da unidade que será alterada (destino)
     and cop.prun_unid_codigo = '002' -- Informar o código da unidade origem dos dados
