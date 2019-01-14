37ede51a3d086c7178f183ff1b7ad717347a555d
Camilo Santos <camilossantos.2809@gmail.com>
2019-01-14 18:09:02 -0300

    Adicionada exclusão de coluna id ao remover primary key

--- remover_primary_key.sql
+++ remover_primary_key.sql
@@ -1,5 +1,10 @@
 /*
-    Script utilizado para remover primary key de todas as tabelas do schema public.
+  Script utilizado para remover primary key de todas as tabelas do schema public e coluna id.
+
+  OBS: Algumas vezes ocorre de acidentalmente atualizar a estrutura do WRpdv ativando a opção de replicação
+  e ser criadas chaves primárias em todas as tabelas do database. 
+  O problema disso é que numa próxima vez em que a estrutura é atualizada e são recriados os índices o WRpdv
+  retorna erro e não consegue concluir o processo, sendo necessária a exclusão da primary key e da coluna id.
 */
 
 do $$
@@ -17,9 +22,13 @@ for linhas in
   where contype = 'p' -- filtro para retornar somente primary key
     and connamespace = 'public'::regnamespace -- filtro para considerar somente as tabelas do schema public
 loop
-   raise notice 'Removendo primary key % da tabela %...', linhas.conname, linhas.tabela;
-   execute format('alter table %I drop constraint %I;', linhas.tabela, linhas.conname);
-   raise notice 'Primary key % da tabela % removida.', linhas.conname, linhas.tabela;
+  raise notice 'Removendo primary key % da tabela %...', linhas.conname, linhas.tabela;
+  execute format('alter table %I drop constraint %I;', linhas.tabela, linhas.conname);
+  raise notice 'Primary key % da tabela % removida.', linhas.conname, linhas.tabela;
+   
+  raise notice 'Removendo coluna id da tabela %...', linhas.tabela;
+  execute format('alter table %I drop column id;', linhas.tabela);
+  raise notice 'Coluna id removida da tabela %.', linhas.tabela;
 end loop;
 
 raise notice 'Script finalizado';
